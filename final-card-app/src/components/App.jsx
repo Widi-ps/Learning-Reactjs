46adecec4abc3943fb6905b361087da559da2b9c
widi-ps <widyapujisaputro@gmail.com>
2024-02-03 09:38:42 +0700

    fixed: add map into the card component to get from contact

--- final-card-app/src/components/App.jsx
+++ final-card-app/src/components/App.jsx
@@ -1,31 +1,26 @@
 import React from "react";
 import Card from "./Card";
-import Contacts from "../contacts.js";
+import Contacts from "../contacts";
 import Avatar from "./Avatar.jsx";
+
+function createCard(props) {
+  return (
+    <Card
+      key={props.id}
+      name={props.name}
+      img={props.imgURL}
+      tel={props.phone}
+      email={props.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://avatars.githubusercontent.com/u/69189062?s=400&u=7746e699fe1b71f1faad015097a226de324e798e&v=4" />
-      <Card
-        name={Contacts[0].name}
-        img={Contacts[0].imgURL}
-        tel={Contacts[0].phone}
-        email={Contacts[0].email}
-      />
-      <Card
-        name={Contacts[1].name}
-        img={Contacts[1].imgURL}
-        tel={Contacts[1].phone}
-        email={Contacts[1].email}
-      />
-      <Card
-        name={Contacts[2].name}
-        img={Contacts[2].imgURL}
-        tel={Contacts[2].phone}
-        email={Contacts[2].email}
-      />
+      {Contacts.map(createCard)}
     </div>
   );
 }

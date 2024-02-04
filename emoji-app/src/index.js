11f984f53ebe60584282d8b539de957e3be73d14
widi-ps <widyapujisaputro@gmail.com>
2024-02-04 18:57:04 +0700

    feature: try to console the truncate with 100 word

--- emoji-app/src/index.js
+++ emoji-app/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import emojipedia from "./emoji";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+const result = emojipedia.map(function (props) {
+  return props.meaning.substring(0, 100);
+});
+
+console.log(result);

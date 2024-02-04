0a6281533d24323de49156d64a2611bdaf23e0b7
widi-ps <widyapujisaputro@gmail.com>
2024-02-04 21:38:27 +0700

    learn to use arrow functions

--- emoji-app/src/index.js
+++ emoji-app/src/index.js
@@ -11,3 +11,27 @@ const result = emojipedia.map(function (props) {
 });
 
 console.log(result);
+
+const numbers = [3, 56, 2, 48, 5];
+
+////Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map((x) => x * 2);
+console.log(newNumbers);
+
+//////Filter - Create a new array by keeping the items that return true.
+const filterNum = numbers.filter((num) => num < 10);
+console.log(filterNum);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+const newNumber = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log(newNumber);
+
+////Find - find the first item that matches from an array.
+const findNumber = numbers.find((num) => num > 10);
+console.log(findNumber);
+
+////FindIndex - find the index of the first item that matches.
+const findIndexNumber = numbers.findIndex((num) => num > 10);
+console.log(findIndexNumber);

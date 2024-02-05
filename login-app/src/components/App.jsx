a763540eacb85ef16e62fdd4059ba0df404300e3
widi-ps <widyapujisaputro@gmail.com>
2024-02-05 09:41:07 +0700

    insert the Login components into the App

--- login-app/src/components/App.jsx
+++ login-app/src/components/App.jsx
@@ -1,18 +1,13 @@
 import React from "react";
+import Login from "./Login";
 
-const isLoggedIn = true;
+const isLoggedIn = false;
 
 function renderConditionally() {
   if (isLoggedIn) {
     return <h1>Hello</h1>;
   } else {
-    return (
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    );
+    return <Login />;
   }
 }
 

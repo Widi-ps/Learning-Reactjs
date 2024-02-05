4066d1865bbc6ebbbcc766c05d0d62b7a6ffdc7e
widi-ps <widyapujisaputro@gmail.com>
2024-02-05 08:56:48 +0700

    add challenge to render all notes

--- keeper-app-2/src/index.js
+++ keeper-app-2/src/index.js
@@ -4,3 +4,6 @@ import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.

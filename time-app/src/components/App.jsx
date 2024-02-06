edfa7307d46263c576b48ddd50b202045f36e743
widi-ps <widyapujisaputro@gmail.com>
2024-02-06 11:10:38 +0700

    implement using the useState and delete am/pm in the time

--- time-app/src/components/App.jsx
+++ time-app/src/components/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const now = new Date().toLocaleTimeString([], { hour12: false });
+  console.log(now);
+
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString([], { hour12: false });
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }

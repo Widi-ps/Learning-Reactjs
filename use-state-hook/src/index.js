2892d5906842704386c26f5720b8bbd6a468b9f7
widi-ps <widyapujisaputro@gmail.com>
2024-02-06 09:56:06 +0700

    change into the App components

--- use-state-hook/src/index.js
+++ use-state-hook/src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div className="container">
-    <h1>0</h1>
-    <button>+</button>
-  </div>
-);
+root.render(<App />);

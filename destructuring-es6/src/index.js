bb37dbf6086fd01121bdb190ee102ad7b199b86f
widi-ps <widyapujisaputro@gmail.com>
2024-02-08 16:27:30 +0700

    try destructuring an arrays with function

--- destructuring-es6/src/index.js
+++ destructuring-es6/src/index.js
@@ -1,18 +1,24 @@
-import animals from "./data";
+import animals, { getAnimals } from "./data";
 
 // Destructuring Arrays
 console.log(animals);
 const [cat, dog] = animals;
 console.log(cat);
 
+const [animal, makeSound] = getAnimals(cat);
+console.log(animal);
+makeSound()
+
 // Destructuring Objects
 const { name, sound } = dog;
-console.log(name, sound);  // dog woof
+console.log(name, sound); // dog woof
 // const { name: catName, sound: catSound } = cat  //change name into catName and sound into catSound
 // console.log(catName, catSound); //cat meow
 // const { name = "Fluffy", sound = "Purr"} = cat // if there is no name and sound it will be assign into Fluffy and Purr
 // console.log(name, sound); // Fluffy Purr
-const {feedingRequirments: {food, water}} = cat
+const {
+  feedingRequirments: { food, water },
+} = cat;
 console.log(food); // 2
 
 // CHALLENGE: uncomment the code below and see the car stats rendered

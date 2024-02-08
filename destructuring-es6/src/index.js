4dc184a311b3233020d920a0b78b8e6ec8cd568c
widi-ps <widyapujisaputro@gmail.com>
2024-02-08 16:15:03 +0700

    learn to destructuring an objects

--- destructuring-es6/src/index.js
+++ destructuring-es6/src/index.js
@@ -5,6 +5,16 @@ console.log(animals);
 const [cat, dog] = animals;
 console.log(cat);
 
+// Destructuring Objects
+const { name, sound } = dog;
+console.log(name, sound);  // dog woof
+// const { name: catName, sound: catSound } = cat  //change name into catName and sound into catSound
+// console.log(catName, catSound); //cat meow
+// const { name = "Fluffy", sound = "Purr"} = cat // if there is no name and sound it will be assign into Fluffy and Purr
+// console.log(name, sound); // Fluffy Purr
+const {feedingRequirments: {food, water}} = cat
+console.log(food); // 2
+
 // CHALLENGE: uncomment the code below and see the car stats rendered
 // import React from "react";
 // import ReactDOM from "react-dom/client";

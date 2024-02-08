7dec5563ca2b1cc6e986e1211ceef5af8a9b1f71
widi-ps <widyapujisaputro@gmail.com>
2024-02-08 16:36:48 +0700

    solve to destructuring cars

--- destructuring-es6/src/index.js
+++ destructuring-es6/src/index.js
@@ -1,47 +1,64 @@
-import animals, { getAnimals } from "./data";
+// import animals, { getAnimals } from "./data";
 
 // Destructuring Arrays
-console.log(animals);
-const [cat, dog] = animals;
-console.log(cat);
+// console.log(animals);
+// const [cat, dog] = animals;
+// console.log(cat);
 
-const [animal, makeSound] = getAnimals(cat);
-console.log(animal);
-makeSound()
+// const [animal, makeSound] = getAnimals(cat);
+// console.log(animal);
+// makeSound()
 
 // Destructuring Objects
-const { name, sound } = dog;
-console.log(name, sound); // dog woof
+// const { name, sound } = dog;
+// console.log(name, sound); // dog woof
 // const { name: catName, sound: catSound } = cat  //change name into catName and sound into catSound
 // console.log(catName, catSound); //cat meow
 // const { name = "Fluffy", sound = "Purr"} = cat // if there is no name and sound it will be assign into Fluffy and Purr
 // console.log(name, sound); // Fluffy Purr
-const {
-  feedingRequirments: { food, water },
-} = cat;
-console.log(food); // 2
+// const {
+//   feedingRequirments: { food, water },
+// } = cat;
+// console.log(food); // 2
 
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import cars from "./practice";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//       <th>Top Colour</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>
-// );
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+} = tesla;
+
+const {
+  coloursByPopularity: [hondaTopColour],
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour],
+} = tesla;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>
+);

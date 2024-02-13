37f71d04dfae4a2091283a228e02815d098c8cb8
widi-ps <widyapujisaputro@gmail.com>
2024-02-13 10:48:26 +0700

    change with a new challenge

--- form-app/src/components/App.jsx
+++ form-app/src/components/App.jsx
@@ -1,63 +1,22 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [fullName, setFullName] = useState({
+  const [contact, setContact] = useState({
     fName: "",
     lName: "",
+    email: ""
   });
-
-  // const [isMouseOver, setMouseOver] = useState(false);
-
-  // function handleClick(event) {
-  //   setHeadingText(name);
-  //   event.preventDefault();
-  // }
-
-  // function handleMouseOver() {
-  //   setMouseOver(true);
-  // }
-
-  // function handleMouseOut() {
-  //   setMouseOver(false);
-  // }
-
-  function handleChange(event) {
-    const { value, name } = event.target;
-
-    setFullName((preValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: preValue.lName,
-        };
-      } else if (name === "lName") {
-        return {
-          fName: preValue.fName,
-          lName: value,
-        };
-      }
-    });
-  }
 
   return (
     <div className="container">
       <h1>
-        Hello {fullName.fName} {fullName.lName}
+        Hello {contact.fName} {contact.lName}
       </h1>
+      <p>{contact.email}</p>
       <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          placeholder="First Name"
-          value={fullName.fname}
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          placeholder="Last Name"
-          value={fullName.lname}
-        />
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <input name="email" placeholder="Email" />
         <button>Submit</button>
       </form>
     </div>
